3cf9c3ec67233b9771214ff3af2928091fcb6222
Raymond Jia <raymond@savvystack.com>
2022-06-23 10:09:16 -0700

    Make the plugin visible again

--- pretix_servicefees/__init__.py
+++ pretix_servicefees/__init__.py
@@ -11,7 +11,7 @@ class PluginApp(AppConfig):
         author = 'Raphael Michel'
         category = 'FEATURE'
         description = gettext_lazy('This plugin allows to charge a service fee on all non-free orders, including Can-Tix customization.')
-        visible = False
+        visible = True
         version = '1.8.0-cantix'
 
     def ready(self):
